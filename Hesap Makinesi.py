e3207ef0aae0e44f73655cbcd1af03446f3931e9
Mustafa Erhan <73031908+MustafaEP@users.noreply.github.com>
2020-11-06 10:45:00 +0300

    Add files via upload

---
+++ Hesap Makinesi.py
@@ -0,0 +1,14 @@
+a=int(input("Ilk Sayiyi Giriniz: "))
+b=int(input("Ikinci Sayiyi Giriniz: "))
+c=float()  #İşlem Kesirli Çıkabilir...
+d=e=f=int()
+
+d=a+b
+e=a-b
+f=a*b
+c=a/b
+
+print("Toplam: ",d)
+print("Çikartma: ",e)
+print("Carpma: ",f)
+print("Bölüm: ",c)
